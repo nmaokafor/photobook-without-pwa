bf7239b35618405fcd6b25c8cdc7cb60ba30602d
Nma <nmaokaforr@yahoo.com>
2020-04-27 22:06:28 +0100

    Re-added CRA default service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './Components/Main';
+import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import {createStore} from 'redux';
 import rootReducer from './redux/reducer';
@@ -10,3 +11,5 @@ import {Provider} from 'react-redux';
 const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><Main/></BrowserRouter></Provider>, document.getElementById('root'));
+
+serviceWorker.unregister();
